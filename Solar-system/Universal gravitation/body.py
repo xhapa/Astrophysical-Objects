23e6f41486f1d6463d4440e5aa7e43a8c74eeb23
Xhapa <emicb0111@gmail.com>
2022-11-14 16:39:52 -0500

    Physics updated

--- Solar-system/Universal gravitation/body.py
+++ Solar-system/Universal gravitation/body.py
@@ -1,25 +1,48 @@
+import numpy as np
 class Body:
-    def __init__(self, mass, size, r, v, a, object_name, colour=None, sun=False):
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 120 / AU  # 1AU = 100 pixels
+    TIMESTEP = 360*24
+    def __init__(self, mass, size, r, v, object_name, colour=None, sun=False):
         self.__mass = mass
         self.__size = size
         self.__r = r
         self.__v = v
-        self.__a = a
+        self.__a = np.array([0, 0], dtype=float)
         self.colour = colour
         self.sun = sun
         self.object_name = object_name
 
     def draw(self, canvas, root):
         canvas.delete(self.object_name)
-        canvas.create_oval(self.pos[0]-(self.size/2),self.pos[1]-(self.size/2),self.pos[0]+(self.size/2),self.pos[1]+(self.size/2), outline = self.colour, fill = self.colour, tags=self.object_name)
+        canvas.create_oval(self.pos[0]*self.SCALE + 700-(self.size/2),self.pos[1]*self.SCALE + 350 -(self.size/2),self.pos[0]*self.SCALE + 700+(self.size/2),self.pos[1]*self.SCALE + 350+(self.size/2), outline = self.colour, fill = self.colour, tags=self.object_name)
         root.update()
 	
     
     def attraction(self, other):
-        pass
+        distance_vec = other.pos - self.pos
+        distance = np.linalg.norm(distance_vec)
+
+        force = self.G * self.mass * other.mass / distance**2
+        theta = np.arctan2(distance_vec[1], distance_vec[0])
+        force_x = np.cos(theta) * force
+        force_y = np.sin(theta) * force
+        return force_x, force_y
 
     def update_position(self, planets):
-        pass
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.vel += [total_fx / self.mass * self.TIMESTEP, total_fy / self.mass * self.TIMESTEP]
+
+        self.pos += self.vel*self.TIMESTEP
 
     @property
     def mass(self):
--- Solar-system/Universal gravitation/main.py
+++ Solar-system/Universal gravitation/main.py
@@ -25,23 +25,24 @@ class App(tk.Tk):
 		self.canvas.pack()	
 
 		self.update_screen()
-		#self.gameon()
+		self.gameon()
 
 	def constants(self):
 		self.bodies = []
-		self.G = 1000.0
-		self.sun_mass = 50.0
+		self.sun_mass = 1.98892 * 10**30
 		self.sun_size = 12
-		self.sun_r = np.array([700, 330])
-		self.sun_v = np.array([0, 0])
-		self.sun_a = np.array([0, 0])
+		self.sun_r = np.array([0, 0], dtype=float)
+		self.sun_v = np.array([0, 0], dtype=float)
 
-		sun = Body(self.sun_mass, self.sun_size, self.sun_r, self.sun_v, self.sun_a,'Sun', 'Yellow', True)
+		sun = Body(self.sun_mass, self.sun_size, self.sun_r, self.sun_v,'Sun', 'Yellow', True)
 		self.bodies.append(sun)
 
 	def init_bodies(self):
-		for i in range(8):
-			body = Body(1.0, 8, np.array([750+50*i, 330]), np.array([0, (-3)**(i+1)]), np.array([0,0]), f'P{i+1}', 'White')
+		masses = [5.9742 * 10**24, 6.39 * 10**23 , 3.30 * 10**23, 4.8685 * 10**24] 
+		vels = [29.783 * 1000, 24.077 * 1000, -47.4 * 1000, -35.02 * 1000]
+		dis = [-1, -1.524, 0.387, 0.723]
+		for i, elem in enumerate(list(zip(masses, vels, dis))):
+			body = Body(elem[0], 8, np.array([elem[2]*Body.AU, 0], dtype=float), np.array([0, elem[1]], dtype=float),f'P{i+1}', 'White')
 			self.bodies.append(body)
 		
 	def center_of_mass(self):
@@ -55,16 +56,16 @@ class App(tk.Tk):
 	
 	def update_screen(self):
 		for body in self.bodies:
+			body.update_position(self.bodies)
 			body.draw(self.canvas, self)
 
-	# def gameon(self): #runs the physics in a loop until told to stop 
-	# 	self.title("Calculating...")
-	# 	i = 0
-	# 	gamestate = 1
-	# 	while gamestate == 1:
-	# 		i += 1
-	# 		self.calculate_trajectories()
-	# 		self.updatescreen()
+	def gameon(self): #runs the physics in a loop until told to stop 
+		self.title("Calculating...")
+		i = 0
+		gamestate = 1
+		while gamestate == 1:
+			i += 1
+			self.update_screen()
 
 def main():
 	app = App()
